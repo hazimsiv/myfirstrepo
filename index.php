1b2f6b1057367299610754f5a6a6b49d5b5ebd4c
hazimsiv <61488701+hazimsiv@users.noreply.github.com>
2021-03-22 23:05:19 +0800

    Create index.php
    my first repo : Welcome to my page html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width initial-">
+  <title>Document</title>
+</head>
+<body>
+  <p> Welcome to my page Junamy Aldridge C. Pedregosa </p>
+</body>
+</html>
